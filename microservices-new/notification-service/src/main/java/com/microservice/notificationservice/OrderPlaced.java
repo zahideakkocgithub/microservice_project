58e2ce35388fc52604e84f0e8b153b6943c6eb02
zahideakkocgithub <44260000+zahideakkocgithub@users.noreply.github.com>
2023-07-02 20:35:01 +0300

    kafka

---
+++ microservices-new/notification-service/src/main/java/com/microservice/notificationservice/OrderPlaced.java
@@ -0,0 +1,15 @@
+package com.microservice.notificationservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class OrderPlaced {
+
+    private String orderNumber;
+}
---
+++ microservices-new/oder-service/src/main/java/com/microservice/orderservice/event/OrderPlaced.java
@@ -0,0 +1,15 @@
+package com.microservice.orderservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class OrderPlaced {
+
+    private String orderNumber;
+}
